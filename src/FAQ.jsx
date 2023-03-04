808e1796ee4b3750fcdf8764337118a8d8931b5d
Oshioke-Salaki <salaki1902@gmail.com>
2023-03-04 10:13:14 -0600

    updated

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import up from './assets/upArrow.png';
 import down from './assets/downArrow.png';
 import './FAQ.css';
@@ -6,13 +6,20 @@ import './FAQ.css';
 export default function FAQ() {
   return (
     <div className="faq">
-      <h3>
+      <h3 className="opened">
         How to find NFT in the NFTWorld marketplace? <img src={down} alt="" />
       </h3>
       <ol className="faqAnswer">
-        <li></li>
-        <li></li>
-        <li></li>
+        <li>
+          On the top left corner of the page, you will see a search bar. Type in
+          the name of the NFT you are looking for, or any keywords related to
+          the NFT.
+        </li>
+        <li>Press enter or click on the search icon to initiate the search.</li>
+        <li>
+          You will be taken to a results page where you can browse through the
+          available NFTs that match your search query.
+        </li>
       </ol>
     </div>
   );
