4feb89acf6c1698b6308effd7dcd2ee578c65f2c
Oshioke-Salaki <salaki1902@gmail.com>
2023-03-04 10:58:12 -0600

    updated

--- src/FAQ.jsx
+++ src/FAQ.jsx
@@ -4,23 +4,32 @@ import down from './assets/downArrow.png';
 import './FAQ.css';
 
 export default function FAQ() {
+  const [opened, setOpened] = useState(false);
   return (
-    <div className="faq">
-      <h3 className="opened">
-        How to find NFT in the NFTWorld marketplace? <img src={down} alt="" />
+    <div className={opened ? 'faq openedFaq' : 'faq closedFaq'}>
+      <h3
+        className={opened ? 'opened' : 'closed'}
+        onClick={() => {
+          setOpened(!opened);
+        }}
+      >
+        How to find NFT in the NFTWorld marketplace?{' '}
+        <img src={opened ? up : down} alt="" />
       </h3>
-      <ol className="faqAnswer">
+      <ul className={opened ? 'faqAnswer openedAnswer' : 'faqAnswer'}>
         <li>
-          On the top left corner of the page, you will see a search bar. Type in
-          the name of the NFT you are looking for, or any keywords related to
+          1. On the top left corner of the page, you will see a search bar. Type
+          in the name of the NFT you are looking for, or any keywords related to
           the NFT.
         </li>
-        <li>Press enter or click on the search icon to initiate the search.</li>
         <li>
-          You will be taken to a results page where you can browse through the
-          available NFTs that match your search query.
+          2. Press enter or click on the search icon to initiate the search.
         </li>
-      </ol>
+        <li>
+          3. You will be taken to a results page where you can browse through
+          the available NFTs that match your search query.
+        </li>
+      </ul>
     </div>
   );
 }
