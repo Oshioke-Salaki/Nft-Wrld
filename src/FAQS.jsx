61901e123272c472e6af8a3d206d4e1c237529d5
Oshioke-Salaki <salaki1902@gmail.com>
2023-03-04 12:20:33 -0600

    updated

--- src/FAQS.jsx
+++ src/FAQS.jsx
@@ -7,13 +7,17 @@ export default function FAQS() {
   return (
     <div className="faqSection">
       <SectionHeader text="FAQS" subText="Checkout Common Questions" />
-      <div className="faqRow">
-        <FAQ question="How to find NFT in the NFTWorld marketplace?" />
-        <FAQ question="How to create NFT in the NFTWorld Marketplace?" />
-        <FAQ question="How to create NFT in the NFTWorld Marketplace?" />
-        <FAQ question="How do i deposit NFT?" />
-        <FAQ question="How do i deposit NFT?" />
-        <FAQ question="How do i deposit NFT?" />
+      <div className="faqss">
+        <div className="faqCol">
+          <FAQ question="How to find NFT in the NFTWorld marketplace?" />
+          <FAQ question="How to create NFT in the NFTWorld Marketplace?" />
+          <FAQ question="How to create NFT in the NFTWorld Marketplace?" />
+        </div>
+        <div className="faqCol">
+          <FAQ question="How do i deposit NFT?" />
+          <FAQ question="How do i deposit NFT?" />
+          <FAQ question="How do i deposit NFT?" />
+        </div>
       </div>
     </div>
   );
